706adffc1b97adc96604a071a14e716ca5e9beaf
Nathan Santiago <santiagonathan9254@gmail.com>
2024-11-13 12:34:07 -0500

    Add units to drawer and change to grid columns.

--- frontend/app/dashboard/menu/page.tsx
+++ frontend/app/dashboard/menu/page.tsx
@@ -53,35 +53,32 @@ const Menu: React.FC = () => {
                             {menuItems.map((item: MenuItem) => (
                                 <Drawer key={item.option_id}>
                                     <DrawerTrigger>{item.option_name}</DrawerTrigger>
+                                    <div className="mx-auto w-full max-w-sm">
                                     <DrawerContent>
                                     <DrawerHeader>
                                     <DrawerTitle>{item.option_name}</DrawerTitle>
-                                    <div className="flex">
-                                        <div>
-                                        <DrawerDescription>Amount Per Serving: {item.AmountPerServing}</DrawerDescription>
-                                        <DrawerDescription>Amount Per Serving ½ cup: {item["AmountPerServing½cup"]}</DrawerDescription>
+                                        <div className='grid grid-cols-2'>
+                                        {/* <DrawerDescription>Amount Per Serving: {item.AmountPerServing}</DrawerDescription>
+                                        <DrawerDescription>Amount Per Serving ½ cup: {item["AmountPerServing½cup"]}</DrawerDescription> */}
                                         <DrawerDescription>Calories: {item.Calories}</DrawerDescription>
-                                        <DrawerDescription>Total Fat: {item.TotalFat}</DrawerDescription>
+                                        <DrawerDescription>Total Fat: {item.TotalFat}g</DrawerDescription>
                                         <DrawerDescription>Saturated Fat: {item.SaturatedFat}</DrawerDescription>
                                         <DrawerDescription>Trans Fat: {item.TransFat}</DrawerDescription>
                                         <DrawerDescription>Cholesterol: {item.Cholesterol}</DrawerDescription>
                                         <DrawerDescription>Sodium: {item.Sodium}</DrawerDescription>
-                                        <DrawerDescription>Total Carbohydrate: {item.TotalCarbohydrate}</DrawerDescription>
-                                        </div>
-                                        <div>
+                                        <DrawerDescription>Total Carbohydrates: {item.TotalCarbohydrate}</DrawerDescription>
                                         <DrawerDescription>Dietary Fiber: {item.DietaryFiber}</DrawerDescription>
                                         <DrawerDescription>Sugars: {item.Sugars}</DrawerDescription>
-                                        <DrawerDescription>Added Sugar: {item.AddedSugar}</DrawerDescription>
-                                        <DrawerDescription>Protein: {item.Protein}</DrawerDescription>
+                                        <DrawerDescription>Added Sugar: {item.AddedSugar ? item.AddedSugar : "0g"}</DrawerDescription>
+                                        <DrawerDescription>Protein: {item.Protein}g</DrawerDescription>
                                         <DrawerDescription>Calcium: {item.Calcium}</DrawerDescription>
-                                        <DrawerDescription>Iron: {item.Iron}</DrawerDescription>
-                                        <DrawerDescription>Potassium: {item.Potassium}</DrawerDescription>
-                                        <DrawerDescription>Vitamin D: {item.VitaminD}</DrawerDescription>
+                                        <DrawerDescription>Iron: {item.Iron}mg</DrawerDescription>
+                                        <DrawerDescription>Potassium: {item.Potassium}mg</DrawerDescription>
+                                        <DrawerDescription>Vitamin D: {item.VitaminD}mcg</DrawerDescription>
                                         <DrawerDescription>Soy: {item.Soy}</DrawerDescription>
                                         <DrawerDescription>Wheat: {item.Wheat}</DrawerDescription>
                                         <DrawerDescription>Sesame: {item.Sesame}</DrawerDescription>
                                         </div>
-                                    </div>
                                     </DrawerHeader>
                                     <DrawerFooter>
                                     <Button>Add to account</Button>
@@ -90,6 +87,7 @@ const Menu: React.FC = () => {
                                     </DrawerClose>
                                     </DrawerFooter>
                                     </DrawerContent>
+                                    </div>
                                 </Drawer>
                             ))}
                         </AccordionContent>
