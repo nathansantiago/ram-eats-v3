88a7fad909b03143afc35fd05b485fd53fdc1167
Nathan Santiago <santiagonathan9254@gmail.com>
2024-11-13 11:02:09 -0500

    Create drawer for individual menu items.

--- frontend/app/dashboard/menu/page.tsx
+++ frontend/app/dashboard/menu/page.tsx
@@ -7,6 +7,17 @@ import {
     AccordionItem,
     AccordionTrigger,
   } from "@/components/ui/accordion"
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+  } from "@/components/ui/drawer"
+import { Button } from "@/components/ui/button"
 import axios from 'axios';
 import { supabase } from "@/lib/supabase";
 import { MenuGetMenuOutput, MenuStations, MenuItem } from '@/app/models/models';
@@ -40,7 +51,46 @@ const Menu: React.FC = () => {
                         <AccordionTrigger>{stationName}</AccordionTrigger>
                         <AccordionContent>
                             {menuItems.map((item: MenuItem) => (
-                                <p key={item.option_id}>{item.option_name}</p>
+                                <Drawer key={item.option_id}>
+                                    <DrawerTrigger>{item.option_name}</DrawerTrigger>
+                                    <DrawerContent>
+                                    <DrawerHeader>
+                                    <DrawerTitle>{item.option_name}</DrawerTitle>
+                                    <div className="flex">
+                                        <div>
+                                        <DrawerDescription>Amount Per Serving: {item.AmountPerServing}</DrawerDescription>
+                                        <DrawerDescription>Amount Per Serving ½ cup: {item["AmountPerServing½cup"]}</DrawerDescription>
+                                        <DrawerDescription>Calories: {item.Calories}</DrawerDescription>
+                                        <DrawerDescription>Total Fat: {item.TotalFat}</DrawerDescription>
+                                        <DrawerDescription>Saturated Fat: {item.SaturatedFat}</DrawerDescription>
+                                        <DrawerDescription>Trans Fat: {item.TransFat}</DrawerDescription>
+                                        <DrawerDescription>Cholesterol: {item.Cholesterol}</DrawerDescription>
+                                        <DrawerDescription>Sodium: {item.Sodium}</DrawerDescription>
+                                        <DrawerDescription>Total Carbohydrate: {item.TotalCarbohydrate}</DrawerDescription>
+                                        </div>
+                                        <div>
+                                        <DrawerDescription>Dietary Fiber: {item.DietaryFiber}</DrawerDescription>
+                                        <DrawerDescription>Sugars: {item.Sugars}</DrawerDescription>
+                                        <DrawerDescription>Added Sugar: {item.AddedSugar}</DrawerDescription>
+                                        <DrawerDescription>Protein: {item.Protein}</DrawerDescription>
+                                        <DrawerDescription>Calcium: {item.Calcium}</DrawerDescription>
+                                        <DrawerDescription>Iron: {item.Iron}</DrawerDescription>
+                                        <DrawerDescription>Potassium: {item.Potassium}</DrawerDescription>
+                                        <DrawerDescription>Vitamin D: {item.VitaminD}</DrawerDescription>
+                                        <DrawerDescription>Soy: {item.Soy}</DrawerDescription>
+                                        <DrawerDescription>Wheat: {item.Wheat}</DrawerDescription>
+                                        <DrawerDescription>Sesame: {item.Sesame}</DrawerDescription>
+                                        </div>
+                                    </div>
+                                    </DrawerHeader>
+                                    <DrawerFooter>
+                                    <Button>Add to account</Button>
+                                    <DrawerClose>
+                                        <Button variant="outline">Close</Button>
+                                    </DrawerClose>
+                                    </DrawerFooter>
+                                    </DrawerContent>
+                                </Drawer>
                             ))}
                         </AccordionContent>
                     </AccordionItem>
