1cb43122a2e23ebd05a8b2ff90979c04886f34df
Nathan Santiago <santiagonathan9254@gmail.com>
2024-11-16 13:13:46 -0500

    Finally fixed chart layer bug!! Put calorie chart into carousel.

--- frontend/app/dashboard/home/page.tsx
+++ frontend/app/dashboard/home/page.tsx
@@ -34,7 +34,7 @@ import { toast } from '@/hooks/use-toast';
 import { useRouter } from 'next/navigation';
 
 const chartData = [
-  { browser: "safari", visitors: 1300, fill: "var(--color-safari)" },
+  { category: "safari", visitors: 1300, fill: "var(--color-safari)" },
 ]
 
 const chartConfig = {
@@ -83,71 +83,89 @@ const HomePage: React.FC = () => {
     return (
         loading ? <LoaderCircle className="mr-2 h-4 w-4 animate-spin" /> : (
         <div className='flex flex-col items-center'>
-            <Card className="flex flex-col max-w-sm w-full">
-                <CardHeader className="items-center pb-0">
-                    <CardTitle className='text-2xl font-bold'>Today's Calorie Goal</CardTitle>
-                    <CardDescription>November 11th 2024</CardDescription>
-                </CardHeader>
-                <CardContent className="flex-1 pb-0">
-                    <ChartContainer
-                    config={chartConfig}
-                    className="mx-auto aspect-square max-h-[250px]"
-                    >
-                    <RadialBarChart
-                        data={chartData}
-                        startAngle={0}
-                        endAngle={chartData[0].visitors/2000*360}
-                        innerRadius={80}
-                        outerRadius={110}
-                    >
-                        <PolarGrid
-                        gridType="circle"
-                        radialLines={false}
-                        stroke="none"
-                        className="first:fill-muted last:fill-background"
-                        polarRadius={[86, 74]}
-                        />
-                        <RadialBar dataKey="visitors" background cornerRadius={10} />
-                        <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
-                        <Label
-                            content={({ viewBox }) => {
-                            if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                                return (
-                                <text
-                                    x={viewBox.cx}
-                                    y={viewBox.cy}
-                                    textAnchor="middle"
-                                    dominantBaseline="middle"
-                                >
-                                    <tspan
-                                    x={viewBox.cx}
-                                    y={viewBox.cy}
-                                    className="fill-foreground text-4xl font-bold"
+            <Carousel 
+                className="max-w-sm w-full"
+                opts={{
+                    align: "start",
+                    loop: true,
+                }}
+            >
+                <CarouselContent>
+                    <CarouselItem key={1}>
+                        <div className="p-1">
+                            <Card className="flex flex-col max-w-sm w-full">
+                                <CardHeader className="items-center pb-0">
+                                    <CardTitle className='text-2xl font-bold'>Today's Calorie Goal</CardTitle>
+                                    <CardDescription>November 11th 2024</CardDescription>
+                                </CardHeader>
+                                <CardContent className="flex-1 pb-0">
+                                    <ChartContainer
+                                    config={chartConfig}
+                                    className="mx-auto aspect-square max-h-[250px]"
                                     >
-                                    {chartData[0].visitors.toLocaleString()}
-                                    </tspan>
-                                    <tspan
-                                    x={viewBox.cx}
-                                    y={(viewBox.cy || 0) + 24}
-                                    className="fill-muted-foreground"
+                                    <RadialBarChart
+                                        data={chartData}
+                                        startAngle={0}
+                                        endAngle={chartData[0].visitors/2000*360}
+                                        innerRadius={80}
+                                        outerRadius={110}
                                     >
-                                    Calories
-                                    </tspan>
-                                </text>
-                                )
-                            }
-                            }}
-                        />
-                        </PolarRadiusAxis>
-                    </RadialBarChart>
-                    </ChartContainer>
-                </CardContent>
-                <CardFooter className="flex-col gap-2 text-sm">
-                    <div className="flex items-center gap-2 font-medium leading-none">
-                    Today's Calorie Goal: 2000
-                    </div>
-                </CardFooter>
-            </Card>
+                                        <PolarGrid
+                                        gridType="circle"
+                                        radialLines={false}
+                                        stroke="none"
+                                        className="first:fill-muted last:fill-background"
+                                        polarRadius={[86, 74]}
+                                        />
+                                        <RadialBar dataKey="visitors" background cornerRadius={10} />
+                                        <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+                                        <Label
+                                            content={({ viewBox }) => {
+                                            if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                                                return (
+                                                <text
+                                                    x={viewBox.cx}
+                                                    y={viewBox.cy}
+                                                    textAnchor="middle"
+                                                    dominantBaseline="middle"
+                                                >
+                                                    <tspan
+                                                    x={viewBox.cx}
+                                                    y={viewBox.cy}
+                                                    className="fill-foreground text-4xl font-bold"
+                                                    >
+                                                    {chartData[0].visitors.toLocaleString()}
+                                                    </tspan>
+                                                    <tspan
+                                                    x={viewBox.cx}
+                                                    y={(viewBox.cy || 0) + 24}
+                                                    className="fill-muted-foreground"
+                                                    >
+                                                    Calories
+                                                    </tspan>
+                                                </text>
+                                                )
+                                            }
+                                            }}
+                                        />
+                                        </PolarRadiusAxis>
+                                    </RadialBarChart>
+                                    </ChartContainer>
+                                </CardContent>
+                                <CardFooter className="flex-col gap-2 text-sm">
+                                    <div className="flex items-center gap-2 font-medium leading-none">
+                                    Today's Calorie Goal: 2000
+                                    </div>
+                                </CardFooter>
+                            </Card>
+                        </div>
+                    </CarouselItem>
+                </CarouselContent>
+                <div className='hidden sm:flex'>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </div>
+            </Carousel>
 
             <Carousel 
                 className="max-w-sm w-full"
--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -9,11 +9,11 @@ export default function DashboardLayout({
   }>) {
     return (
         <div className='flex flex-col min-h-screen min-w-screen p-4'>
-            <div className='fixed top-0 left-0 px-4 pb-2 border-b w-full backdrop-filter backdrop-blur-lg bg-opacity-30'><Header /></div>
+            <div className='fixed top-0 left-0 px-4 pb-2 border-b w-full backdrop-filter backdrop-blur-lg bg-opacity-30 z-10'><Header /></div>
             <main className='my-[50px] flex-grow justify-center flex items-center max-w-full max-h-full overflow-auto'>
                 {children}
             </main>
-            <div className='justify-center flex items-center fixed bottom-0 left-[50%] translate-x-[-50%] backdrop-filter backdrop-blur-lg bg-opacity-30 w-full z-45 border-t'><Footer /></div>
+            <div className='justify-center flex items-center fixed bottom-0 left-[50%] translate-x-[-50%] backdrop-filter backdrop-blur-lg bg-opacity-30 w-full z-10 border-t'><Footer /></div>
         </div>
     );
 };
