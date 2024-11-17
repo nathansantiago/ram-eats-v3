d6190c69b725749735474a67fc61c3b7183e85af
Joel D'Silva <jjdsilva@unc.edu>
2024-11-16 21:17:24 -0500

    small adjustment to meal_calculation module (tweaking threshold levels and clearing comments)

--- backend/src/modules/meal_calculation/module.py
+++ backend/src/modules/meal_calculation/module.py
@@ -76,12 +76,10 @@ class MealCalculationModule:
         return merged_menu_data
 
     def categorize_items(self, menu: list[dict[str, any]]) -> list[dict[str, any]]:
-        # Function to convert nutrient values to numbers
         def convert_to_float(value: str) -> float:
             numeric_value = re.findall(r"[-+]?\d*\.\d+|\d+", value)
             return float(numeric_value[0]) if numeric_value else 0.0
 
-        # Convert nutrient values to numbers
         for item in menu:
             item["Protein"] = convert_to_float(item.get("Protein", "0"))
             item["Calories"] = convert_to_float(item.get("Calories", "0"))
@@ -90,7 +88,7 @@ class MealCalculationModule:
             item["Potassium"] = convert_to_float(item.get("Potassium", "0"))
             item["VitaminD"] = convert_to_float(item.get("VitaminD", "0"))
 
-        # Categorize items based on healthiness rating
+        # Function sorts menu items based on "healthiness" level
         categorized_menu = sorted(menu, key=lambda x: (
             -x["Protein"] / x["Calories"] if x["Calories"] > 0 else 0,
             x["TotalFat"],
@@ -101,30 +99,23 @@ class MealCalculationModule:
         return categorized_menu
 
     def calculate_daily_cal(self, user_data: dict[str, any]) -> int:
-        # Calculate BMR using Mifflin-St Jeor Equation
-        user_data['height'] *= 2.54  # Convert height from inches to cm
-        user_data['weight'] *= 0.453592  # Convert weight from lbs to kg
+        user_data['height'] *= 2.54
+        user_data['weight'] *= 0.453592
 
         pal = user_data['activity_level'] / 10
 
         if user_data['gender'] == False:
-            # Calculate BMR for males
             bmr = 10 * user_data['weight'] + 6.25 * user_data['height'] - 5 * user_data['age'] + 5
         else:
-            # Calculate BMR for females
             bmr = 10 * user_data['weight'] + 6.25 * user_data['height'] - 5 * user_data['age'] - 161
         
-        # Calculate maintenance calories using BMR and activity level
         maintenance_cal = round(bmr * pal)
 
         if user_data['fitness_goal'] == 0:
-            # Calculate daily caloric intake for bulking
             daily_tot_cal = round(maintenance_cal * 1.11)
         elif user_data['fitness_goal'] == 2:
-            # Calculate daily caloric intake for cutting
             daily_tot_cal = round(maintenance_cal * 0.89)
         else:
-            # Daily calories for maintaining weight
             daily_tot_cal = maintenance_cal
 
         return daily_tot_cal
@@ -132,10 +123,10 @@ class MealCalculationModule:
     def calculate_meal(self, user_data: dict[str, any], menu: list[dict[str, any]]) -> dict[str, any]:
         daily_cal = self.calculate_daily_cal(user_data)
         meal_cal = daily_cal // (user_data['meal_count'])
-        meal_cal_lower = meal_cal - 50
-        meal_cal_upper = meal_cal + 50
-        protein_goal = 0.25 * meal_cal_upper  # Slightly reduced protein percentage
-        carbs_goal = 0.50 * meal_cal_upper  # Adjusted carbohydrate percentage
+        meal_cal_lower = meal_cal - 25
+        meal_cal_upper = meal_cal + 25
+        protein_goal = 0.25 * meal_cal_upper
+        carbs_goal = 0.50 * meal_cal_upper
         fats_goal = 0.25 * meal_cal_upper
 
         selected_items = {}
@@ -179,7 +170,6 @@ class MealCalculationModule:
                     total_carbs += info["TotalCarbohydrate"]
                     total_fats += info["TotalFat"]
 
-                    # Break the loop if we are within the target range
                     if total_calories >= meal_cal_lower:
                         break
 
